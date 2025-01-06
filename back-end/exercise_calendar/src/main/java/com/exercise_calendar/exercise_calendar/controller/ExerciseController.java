e92aa8cac1672aec82ac89b20a9d19364d61baa3
Juri-Jang <jjr84@dgu.ac.kr>
2025-01-06 17:59:39 +0900

    [BE] 폴더명 수정

--- back-end/exercise_calendar/src/main/java/com/exercise_calendar/exercise_calendar/controller/ExerciseController.java
+++ back-end/exercise_calendar/src/main/java/com/exercise_calendar/exercise_calendar/controller/ExerciseController.java
@@ -1,16 +1,12 @@
 package com.exercise_calendar.exercise_calendar.controller;
 
-import com.exercise_calendar.exercise_calendar.dto.ExerciseReqDto;
-import com.exercise_calendar.exercise_calendar.dto.ExerciseResDto;
-import com.exercise_calendar.exercise_calendar.entity.Exercise;
+import com.exercise_calendar.exercise_calendar.dto.createReqDto;
+import com.exercise_calendar.exercise_calendar.dto.createResDto;
 import com.exercise_calendar.exercise_calendar.service.ExerciseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/exercise")
@@ -19,9 +15,9 @@ public class ExerciseController {
     private ExerciseService exerciseService;
 
     @PostMapping("/create")
-    public ResponseEntity<?> register(@RequestBody ExerciseReqDto dto) {
+    public ResponseEntity<?> register(@RequestBody createReqDto dto) {
         try {
-            ExerciseResDto exercise = exerciseService.create(dto);
+            createResDto exercise = exerciseService.create(dto);
             return ResponseEntity.status(HttpStatus.OK).body(exercise);
         } catch (IllegalArgumentException e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
@@ -29,4 +25,6 @@ public class ExerciseController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("서버 에러가 발생했습니다.");
         }
     }
+
+
 }
